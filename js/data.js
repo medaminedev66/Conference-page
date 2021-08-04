829bda05ca7e1c0b9583da6f07061592b9b4a4d2
medaminedev66 <md.amine.smahi@gmail.com>
2021-08-04 14:31:33 +0100

    Add speaker' image, create the second section of the footer

---
+++ js/data.js
@@ -0,0 +1,56 @@
+var speakers = [
+  {
+    name: "Yochai Benkler",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/alex-suprun-ZHvM3XIOHoE-unsplash.jpg",
+    alt: "Photo by Christopher Campbell on Unsplash",
+  },
+  {
+    name: "Ali Abbani",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/nicolas-horn-MTZTGvDsHFY-unsplash.jpg",
+    alt: "",
+  },
+  {
+    name: "Henry hi",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/aatik-tasneem-7omHUGhhmZ0-unsplash.jpg",
+    alt: "",
+  },
+  {
+    name: "Julina Hey",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/christopher-campbell-rDEOVtE7vOs-unsplash.jpg",
+    alt: "Photo by Christopher Campbell on Unsplash",
+  },
+  {
+    name: "Julina Hey",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/jack-finnigan-rriAI0nhcbc-unsplash.jpg",
+    alt: "Photo by Jack Finnigan on Unsplash",
+  },
+  {
+    name: "Julina Hey",
+    proffession:
+      "Berkman professor of Entreneurial Legal studies at harvard Law school",
+    description:
+      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
+    photo: "/img/profiles/tamarcus-brown-29pFbI_D1Sc-unsplash.jpg",
+    alt: "Photo by Tamarcus Brown on Unsplash",
+  },
+];
--- js/index.js
+++ js/index.js
@@ -13,42 +13,7 @@ document.querySelector(".fa-bars").addEventListener("click", openNav);
 document.querySelector(".closeBtn>i").addEventListener("click", closeMenu);
 document.querySelector("li > a").addEventListener("click", closeMenu);
 
-var speakers = [
-  {
-    name: "Yochai Benkler",
-    proffession:
-      "Berkman professor of Entreneurial Legal studies at harvard Law school",
-    description:
-      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
-    photo: "/img/profiles/IMG.jpg",
-  },
-  {
-    name: "Ali Abbani",
-    proffession:
-      "Berkman professor of Entreneurial Legal studies at harvard Law school",
-    description:
-      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
-    photo: "/img/profiles/IMG.jpg",
-  },
-  {
-    name: "Henry hi",
-    proffession:
-      "Berkman professor of Entreneurial Legal studies at harvard Law school",
-    description:
-      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
-    photo: "/img/profiles/IMG.jpg",
-  },
-  {
-    name: "Julina Hey",
-    proffession:
-      "Berkman professor of Entreneurial Legal studies at harvard Law school",
-    description:
-      " bekler studies commons-based Berkman professor of Entreneurial Legal studies at harvard Law school",
-    photo: "/img/profiles/IMG.jpg",
-  },
-];
-
-for (let i = 0; i < 2; i++) {
+const generateSpeaker = (index) => {
   var container = document.createElement("div");
   var image = document.createElement("div");
   var chess = document.createElement("img");
@@ -76,57 +41,36 @@ for (let i = 0; i < 2; i++) {
   information.className = "information";
 
   document.querySelector(".speakers").appendChild(container);
-  fullname.innerText = speakers[i].name;
+  fullname.innerText = speakers[index].name;
   chess.src = "./img/profiles/Chess_board.svg.png";
-  photo.src = speakers[i].photo;
-  // photo.alt = speakers[i].alt;
-  proffession.innerText = speakers[i].proffession;
-  description.innerText = speakers[i].description;
+  photo.src = speakers[index].photo;
+  photo.alt = speakers[index].alt;
+  proffession.innerText = speakers[index].proffession;
+  description.innerText = speakers[index].description;
+};
+
+if (window.innerWidth <= 700) {
+  for (let i = 0; i < 2; i++) {
+    generateSpeaker(i);
+  }
+} else {
+  for (let i = 0; i < speakers.length; i++) {
+    generateSpeaker(i);
+  }
 }
 
-var second = false;
 const openMore = () => {
-  for (let i = 2; i < speakers.length; i++) {
-    var container = document.createElement("div");
-    var image = document.createElement("div");
-    var chess = document.createElement("img");
-    var photo = document.createElement("img");
-    var fullname = document.createElement("h3");
-    var proffession = document.createElement("h4");
-    var description = document.createElement("p");
-    var information = document.createElement("div");
-
-    container.appendChild(image);
-    container.appendChild(information);
-    image.appendChild(chess);
-    image.appendChild(photo);
-    information.appendChild(fullname);
-    information.appendChild(proffession);
-    information.appendChild(description);
-
-    container.className = "container";
-    image.className = "big-image";
-    chess.className = "chess-image";
-    photo.className = "profile-image";
-    fullname.className = "full-name";
-    proffession.className = "profession";
-    description.className = "description";
-    information.className = "information";
-
-    document.querySelector(".speakers").appendChild(container);
-    fullname.innerText = speakers[i].name;
-    chess.src = "./img/profiles/Chess_board.svg.png";
-    photo.src = speakers[i].photo;
-    // photo.alt = speakers[i].alt;
-    proffession.innerText = speakers[i].proffession;
-    description.innerText = speakers[i].description;
-  }
   // document.querySelector(".more").classList.add("invisible");
   // var about = document.querySelector(".about");
   // document.querySelector(".moremore").appendChild(about);
+  for (let i = 2; i < speakers.length; i++) {
+    generateSpeaker(i);
+  }
   document.querySelector(".more").removeEventListener("click", openMore);
   second = true;
 };
+
+var second = false;
 var count = 0;
 document.querySelector(".more").addEventListener("click", openMore);
 document.querySelector(".more").addEventListener("click", () => {
